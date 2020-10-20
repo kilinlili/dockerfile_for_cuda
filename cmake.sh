b169d91489dd906124b82f4ad22b1b673569ea2f
kilin <rin.ueno@cpc.ait.kyushu-u.ac.jp>
2020-10-21 02:43:54 +0900

    Run cmake.sh, Error test_program

--- cmake.sh
+++ cmake.sh
@@ -5,29 +5,26 @@ OPENCV_VERSION='4.4.0'
 
 cmake \
         -DBUILD_EXAMPLES=ON \
-        -DBUILD_opencv_python2=ON \
         -DBUILD_opencv_python3=ON \
         -DCMAKE_BUILD_TYPE=RELEASE \
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCUDA_ARCH_BIN=5.3,6.2,7.2 \
+        -DCUDA_ARCH_BIN=7.2,7.5 \
         -DCUDA_ARCH_PTX= \
-        -DCUDNN_VERSION='8.0' \
         -DEIGEN_INCLUDE_PATH=/usr/include/eigen3 \
-        -DOPENCV_ENABLE_NONFREE=ON \
-        -DOPENCV_EXTRA_MODULES_PATH=/opt/opencv-4.4.0/opencv_contrib-4.4.0/modules \
+        -DOPENCV_EXTRA_MODULES_PATH=/opt/opencv/opencv_contrib/modules \
         -DOPENCV_GENERATE_PKGCONFIG=ON \
-        -DWITH_CUBLAS=ON \
         -DWITH_CUDA=ON \
+        -DWITH_OPENGL=ON \
         -DWITH_GSTREAMER=ON \
         -DWITH_LIBV4L=ON \
         -DWITH_OPENGL=ON \
         -DWITH_TBB=ON \
         -DWITH_V4L=ON \
         -DWITH_FFMPEG=ON \
-        -DOPENCV_DNN_CUDA=ON \
-        -DBUILD_opencv_world=OFF \
         .. 2>&1 | tee -a config.log
 
 
-        #-DENABLE_NEON=ON \
-        #この命令はarm用(armv7,8両方で使えるとかは未確認)
+        #-DCUDA_ARCH_PTXは空欄にしておかないとmakeができない
+        #一応このスクリプトでビルドができた．
+        # 450 cuda-11.0(10を入れようとしたところ失敗してそのまま11にバージョンが変わった)
+        
